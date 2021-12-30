3daca5704d5a289bebdeff244462cc5d09fb499d
Claudio <94421810+ClaLobos@users.noreply.github.com>
2021-12-30 14:29:38 -0300

    Add files via upload

---
+++ viajes_chile/assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    // FUNCION SMOOTH SCROLL
+    $(".navbar a").click(function(event){
+        event.preventDefault();
+        var gato = this.hash;
+        $("html").animate(
+            {
+                scrollTop: $(gato).offset().top,
+            },
+            800
+        );
+    });
+
+});
+
+
+
+// FUNCION PARA CARGAR LOS TOOLTIP
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
